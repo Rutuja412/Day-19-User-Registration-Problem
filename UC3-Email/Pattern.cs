54804a8058b119a304e467d34e66e3269642b1f5
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-20 14:00:57 +0530

    [Valid EmailId of user]

---
+++ UC3-Email/Pattern.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UC3_Email
+{
+    internal class Pattern
+    {
+        //uc1-FirstNAME
+        public static string FIRSTNAME = ("^[A-Z]{1,} [a-z]{2,}$");
+        public bool validate_FirstName(string firstName)
+        {
+            return Regex.IsMatch(FIRSTNAME, firstName);
+
+        }
+
+        //UC2
+        public static string LASTNAME = ("^[A-Z]{1}[a-z]{2,}$");
+        public bool validate_LastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, LASTNAME);
+        }
+
+        //UC3
+        public static string EMAILID = ("^[a-zA-Z0-9]+ ([._-][a-zA-Z0-9]+)* @ [a-z]+ ([.][a-z])* ([.][a-z]+)$");
+        public bool validate_EmailId(string emailId)
+        {
+            return Regex.IsMatch(EMAILID, emailId);
+        }
+
+    }
+}
---
+++ UC3-Email/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace UC3_Email
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration");
+            Pattern p = new Pattern();
+            //UC1
+            Console.WriteLine("Enter first name");
+            string firstName = Console.ReadLine();
+            bool name = p.validate_FirstName(firstName);
+            if (name)
+            {
+                Console.WriteLine("Correct");
+
+            }
+            else
+            {
+                Console.WriteLine("Not Matching");
+            }
+
+            //UC2
+            Console.WriteLine("Enter last name ");
+            string lastName = Console.ReadLine();
+            bool Lname = p.validate_LastName(lastName);
+            if (Lname)
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Not matching");
+            }
+
+            //UC3
+            Console.WriteLine("Enter EmailId");
+            string emailId = Console.ReadLine();
+            bool mailId = p.validate_EmailId(emailId);
+            if (mailId)
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Not Matching");
+            }
+
+        }
+    }
+}
