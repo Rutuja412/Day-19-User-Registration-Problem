94e0302a75bcf8b2c788dde5fd5f6496e0182a8c
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-20 13:37:09 +0530

    [Valid First Name]

---
+++ UC-1 FirstName/Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UC_1_FirstName
+{
+    internal class Pattern
+    {
+        //uc1-FirstNAME
+        public static string FIRSTNAME = ("^[A-Z]{1,} [a-z]{2,}$");
+        public bool validate_FirstName(string firstName)
+        {
+            return Regex.IsMatch(FIRSTNAME, firstName);
+
+        }
+
+    }
+}
---
+++ UC-1 FirstName/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UC_1_FirstName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration");
+            Pattern p = new Pattern();
+            Console.WriteLine("Enter first name");
+            string firstName = Console.ReadLine();
+            bool name = p.validate_FirstName(firstName);
+            if (name)
+            {
+                Console.WriteLine("Correct");
+
+            }
+            else
+            {
+                Console.WriteLine("Not Matching");
+            }
+
+        }
+    }
+}
